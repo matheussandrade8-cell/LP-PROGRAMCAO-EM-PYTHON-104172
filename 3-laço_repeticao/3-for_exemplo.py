7ab32806838271c67d30f08f795d23fd25950c41
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:57:17 -0300

    atividade de laço_repeticao

---
+++ 3-laço_repeticao/3-for_exemplo.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+for i in range(10,0,-1):
+    print(i)
+
+
+print("fim")
---
+++ 3-laço_repeticao/6-exercicio.py
@@ -0,0 +1,11 @@
+import os
+import time
+os.system("cls")
+n=int(input("digite o numero:"))
+
+for i in range(n,-1,-1):
+    print(i)
+    time.sleep(0.5)
+
+
+print("aperta f no chat")
